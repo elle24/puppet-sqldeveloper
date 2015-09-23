3b33fbfff15f297364d3d466b68d96280474ec7f
elle24 <femee.canlas@gmail.com>
2015-09-23 14:03:22 +0800

    Create Puppet for SQL Developer

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
-# Public: Install Keynote for TORO only
+# Public: Install SQLDeveloper for TORO only
 #
 # Usage:
 #
-#   include keynote
-class keynote {
-  package { 'Keynote':
-    source   => 'http://192.168.21.151/Keynote.zip',
+#   include sqldeveloper
+class sqldeveloper {
+  package { 'SQLdeveloper':
+    source   => 'http://192.168.21.151/sqldeveloper-4.1.1.19.59-macosx.app.zip',
     provider => 'compressed_app'
   }
 }
---
+++ spec/classes/sqldeveloper_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+#describe 'sqldeveloper' do
+#  it do
+#    should contain_package('SQLdeveloper').with({
+#      :source   => 'http://192.168.21.151/sqldeveloper-4.1.1.19.59-macosx.app.zip',
+#      :provider => 'compressed_app'
+#    })
+#  end
+#end
